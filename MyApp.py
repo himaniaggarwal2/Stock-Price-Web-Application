ef3f07f82ed62fe6e2eeeb92523e5bc84f7f5466
himaniaggarwal2 <himaniaggarwal2@gmail.com>
2020-11-17 16:05:22 +0530

    changed the design

--- MyApp.py
+++ MyApp.py
@@ -5,7 +5,7 @@ st.write("""
 # Simple Stock Price App
 Shown are the stock closing price and volume of Boeing Company!
 """)
-
+        
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 #define the ticker symbol
 tickerSymbol = 'BA'
@@ -14,14 +14,20 @@ tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
-
+st.write("""#Stock Price""")
 st.line_chart(tickerDf.Close)
+
+st.write("""# Volume""")
 st.line_chart(tickerDf.Volume)
 
-#info on the company
-tickerData.info
-
+st.write("""# Calendar""")
 tickerData.calendar
 
+
+st.write("""# Broker Recommendations""")
 #get recommendation data for ticker
 tickerData.recommendations
+
+st.write("""# Company Info.""")
+#info on the company
+tickerData.info
