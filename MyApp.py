8baf400922f7b20ecb5f1961a77c5b08da8708be
himaniaggarwal2 <himaniaggarwal2@gmail.com>
2020-11-17 16:33:15 +0530

    changes title

--- MyApp.py
+++ MyApp.py
@@ -14,7 +14,7 @@ tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
-st.write("""#Stock Price""")
+st.write("""# Stock Price""")
 st.line_chart(tickerDf.Close)
 
 st.write("""# Volume""")
